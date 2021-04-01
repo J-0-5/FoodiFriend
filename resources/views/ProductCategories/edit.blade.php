e966d04b939e5c43c8478dee7e43ee924558acb8
Jos <jose20010822@gmail.com>
2021-04-01 16:39:34 -0500

    include edit

--- resources/views/ProductCategories/edit.blade.php
+++ resources/views/ProductCategories/edit.blade.php
@@ -1,9 +1,50 @@
-<h1>@lang('Create Categories')</h1>
+{{-- <h1>@lang('Create Categories')</h1> --}}
 
-<ul>
-    <li><a href="{{route('productCategory.index')}}">@lang('Show Categories')</a></li>
+{{-- <ul> --}}
+    {{-- <li><a href="{{route('productCategory.index')}}">@lang('Show Categories')</a></li> --}}
     {{--
     <li><a href="{{route('productCategory.edit')}}">@lang('Update Categories')</a></li>
     <li><a href="{{route('productCategory.delete')}}">@lang('Delete Categories')</a></li>
     --}}
-</ul>
+{{-- </ul> --}}
+<div class="modal fade" id="createCategories">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header bg-info text-white">
+                <h4 class="modal-title">@lang('Create Categories')</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <form action="{{ route('productCategory.store') }}" method="POST" class="was-validated">
+                @csrf
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>@lang('Name')</label>
+                        <input type="text" class="form-control" name="name" required />
+                    </div>
+                    <div class="form-group">
+                        <label>@lang('State')</label>
+                        <select class="form-control" name="state" required>
+                            <option value="">@lang('Select')</option>
+                            <option value="1">Activo</option>
+                            <option value="2">Inactivo</option>
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">
+                        <span>Enviar</span>
+                    </button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">
+                        <span>Cerrar</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- resources/views/ProductCategories/index.blade.php
+++ resources/views/ProductCategories/index.blade.php
@@ -4,53 +4,13 @@
 
 <div class="container-fluid px-3 mb-3">
 
-   {{--  @yield('modalCreate') --}}
+    {{--  @yield('modalCreate') --}}
 
-<button type="button" class="btn btn-primary ml-5 mb-3" data-toggle="modal" data-target="#createCategories">
-   @lang('Create Categories')
-</button>
+    <button type="button" class="btn btn-primary ml-5 mb-3" data-toggle="modal" data-target="#createCategories">
+        @lang('Create Categories')
+    </button>
 
-<div class="modal fade" id="createCategories">
-<div class="modal-dialog modal-md">
-   <div class="modal-content">
-
-       <!-- Modal Header -->
-       <div class="modal-header bg-info text-white">
-           <h4 class="modal-title">@lang('Create Categories')</h4>
-           <button type="button" class="close" data-dismiss="modal">&times;</button>
-       </div>
-
-       <form action="{{ route('productCategory.store') }}" method="POST" class="was-validated">    
-       @csrf
-           <!-- Modal body -->
-           <div class="modal-body">
-               <div class="form-group">
-                   <label>@lang('Name')</label>
-                   <input type="text" class="form-control" name="name" required/>
-               </div>
-               <div class="form-group">
-                   <label>@lang('State')</label>
-                   <select class="form-control" name="state" required>
-                       <option value="">@lang('Select')</option>
-                       <option value="1">Activo</option>
-                       <option value="2">Inactivo</option>
-                   </select>
-               </div>
-           </div>
-
-           <!-- Modal footer -->
-           <div class="modal-footer">
-               <button type="submit" class="btn btn-primary">
-                   <span>Enviar</span>
-               </button>
-               <button type="button" class="btn btn-danger" data-dismiss="modal">
-                   <span>Cerrar</span>
-               </button>
-           </div>
-       </form>
-   </div>
-</div>
-</div>
+    @include('ProductCategories.edit')
 
 
     <div class="row px-3">
@@ -60,7 +20,7 @@
         </div>
     </div>
 
-    
+
 
     @if($errors->any())
     <div class="alert alert-danger">
@@ -123,8 +83,8 @@
                         </div>
                         <div class="col-6">
                             <div class="form-group">
-                                <a href="{{ route('productCategory.index') }}"
-                                    class="btn btn-primary btn-block"><i class="fas fa-backspace"></i> Borrar</a>
+                                <a href="{{ route('productCategory.index') }}" class="btn btn-primary btn-block"><i
+                                        class="fas fa-backspace"></i> Borrar</a>
                             </div>
                         </div>
                     </div>
