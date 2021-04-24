f5aef5fcb3318574afd122dd91bc234327b5b042
jdatuesta7 <jdatuesta7@gmail.com>
2021-04-24 15:47:18 -0500

    fixed style in order.details

--- resources/views/Order/details.blade.php
+++ resources/views/Order/details.blade.php
@@ -5,13 +5,13 @@
 @foreach ($order as $order)
 <div class="container-fluid px-3 mb-3">
     <div class="row">
-        <div class="col-12">
+        <div class="col">
             <div class="card-deck">
-                <div class="card col-8">
+                <div class="card col-md-8 col-sm-12">
                     <div class="card-body">
                         <div class="container">
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-md-6 col-sm-12">
                                     <h3 class="card-title mb-4">{{__('Order').' #'.$id}} <i class="fas fa-shopping-cart"></i></h3>
                                     <h5 class="card-text text-info">{{__('Name')}}</h5>
                                     <p class="card-text">{{$order->getUser->name}}</p>
@@ -20,7 +20,7 @@
                                     <h5 class="card-text text-info">{{__('Commerce')}}</h5>
                                     <p class="card-text">{{$order->getCommerce->name}}</p>
                                 </div>
-                                <div class="col-6 mt-5">
+                                <div class="col-md-6 col-sm-12 mt-5">
                                     <h5 class="card-text text-info">{{__('Email')}}</h5>
                                     <p class="card-text">{{$order->getUser->email}}</p>
                                     <h5 class="card-text text-info">{{__('Address')}}</h5>
@@ -36,7 +36,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="card col-4">
+                <div class="card col-md-4 col-sm-12">
                     <div class="card-body">
                     <h3 class="card-title mt-4">{{__('Invoice Detail')}}</h3>
                         <div class="container">
@@ -60,49 +60,51 @@
             </div>
         </div>
     </div>
-    
-    @foreach($orderDetails as $orderDetail)
-    <div class="row px-3 mt-3">
-        <div class="col-12 mr-n1 d-flex">
-            <div class="card-deck">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-2">
-                                <img class="card-img-top img-fluid rounded" src="{{asset('img/product-placeholder.jpg')}}" alt="Card image cap">
-                            </div>
-                            <div class="col-10 ">
-                                <div class="row">
-                                    <div class="col-9">
-                                        <h5 class="card-title mt-4">{{$orderDetail->getProduct->name}}</h5>
-                                        <p class="card-text">{{__('Quantity')}}: {{$orderDetail->quantity}}</p>
-                                        {{-- <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae modi dolor sed atque cum, et incidunt enim blanditiis nemo nesciunt esse ea. Nostrum labore repellat, corporis accusamus alias illo in!</p> --}}
-                                    </div>
-                                    <div class="col-3 mt-4">
-                                        <p class="card-text">&#36;{{number_format($orderDetail->getProduct->price*$orderDetail->quantity,2)}}</p>
-                                    </div>
+    <div class="row">
+        <div class="col-md-8 col-sm-12">
+            <div class="card mt-4 mr-4">
+                <div class="card-header">
+                    <h3>{{__('Products')}}</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                @foreach($orderDetails as $orderDetail)
+                    <li class="list-group-item" >
+                    <div class="row">
+                        <div class="col-md-2 col-sm-2 mt-4">
+                            <img class="card-img-top img-fluid rounded" src="{{asset('img/product-placeholder.jpg')}}" alt="Card image cap">
+                        </div>
+                        <div class="col-md-10 col-sm-10">
+                            <div class="row">
+                                <div class="col-md-9 col-sm-10 mt-2">
+                                    <h5 class="card-title mt-4">{{$orderDetail->getProduct->name}}</h5>
+                                    <ss="col-3 mt-4">
+                                    <p cp class="card-text">{{__('Quantity')}}: {{$orderDetail->quantity}}</p>
+                                    {{-- <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae modi dolor sed atque cum, et incidunt enim blanditiis nemo nesciunt esse ea. Nostrum labore repellat, corporis accusamus alias illo in!</p> --}}
+                                </div>
+                                <div class="col-md-3 col-sm-2 mt-4">&#36;{{number_format($orderDetail->getProduct->price*$orderDetail->quantity,2)}}</p>
                                 </div>
                             </div>
-                        </div> 
-                    </div>
-                </div>
+                        </div>
+                    </div> 
+                    </li>
+                @endforeach
+                </ul>
             </div>
         </div>
-    </div> 
-    @endforeach
-    <div class="row px-3 mt-3">
-        <div class="card-deck">
-            <div class="card col">
-                <div class="card-body">
-                    <h3 class="card-title mt-4">{{__('Estado del pedido')}}</h3>
-                    <h5 class="card-text text-info">{{__('Status')}}</h5>
-                    @foreach(Config::get('const.status') as $status => $value)
-                        @if($order->status == $status)
-                            <p class="card-text">{{$value['name']}}</p>
-                            @break
-                        @endif
-                    @endforeach
-                    <a href="#" class="btn btn-danger mb-3">{{__('Order Delete')}}</a>
+        <div class="col-md-4 col-sm-12 mt-1 pl-2">
+            <div class="row px-3 mt-3">
+                <div class="card col-md-12">
+                    <div class="card-body">
+                        <h3 class="card-title mt-4">{{__('Estado del pedido')}}</h3>
+                        <h5 class="card-text text-info">{{__('Status')}}</h5>
+                        @foreach(Config::get('const.status') as $status => $value)
+                            @if($order->status == $status)
+                                <p class="card-text">{{$value['name']}}</p>
+                                @break
+                            @endif
+                        @endforeach
+                        <a href="#" class="btn btn-danger mb-3">{{__('Order Delete')}}</a>
+                    </div>
                 </div>
             </div>
         </div>
